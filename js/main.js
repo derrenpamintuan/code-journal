d675f664af2273ec700f7b762a70a1361f2fd4bd
Derren Pamintuan <pamintuan2000@gmail.com>
2023-04-11 05:30:18 +0000

    Add an 'input' event listener to the Photo URL input that sets the src attribute of the photo preview to the input's value when the user types or pastes in a new URL. #1

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const $photoURL = document.querySelector('.url');
+const $image = document.querySelector('img');
+$photoURL.addEventListener('input', setSRC);
+
+function setSRC(event) {
+  $image.setAttribute('src', event.target.value);
+  if (event.target.value === '') {
+    $image.setAttribute('src', 'images/placeholder-image-square.jpg');
+  }
+}
