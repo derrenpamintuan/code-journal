a75e1f8f9123a038c97d667ad74ea7b152e859ab
Derren Pamintuan <pamintuan2000@gmail.com>
2023-04-11 18:30:17 +0000

    Completed callback function for event listener. #1

--- js/main.js
+++ js/main.js
@@ -9,9 +9,21 @@ function setSRC(event) {
   }
 }
 
-const $save = document.querySelector('.save');
-$save.addEventListener('submit', handleSubmit);
+const $form = document.querySelector('form');
+$form.addEventListener('submit', handleSubmit);
 
 function handleSubmit(event) {
   event.preventDefault();
+
+  const entry = {
+    entryId: data.nextEntryId,
+    title: $form.elements.title.value,
+    url: $form.elements.photoURL.value,
+    notes: $form.elements.notes.value
+  };
+
+  data.nextEntryId++;
+  data.entries.push(entry);
+  $image.setAttribute('src', 'images/placeholder-image-square.jpg');
+  $form.reset();
 }
